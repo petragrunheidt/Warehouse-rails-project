d1f5303208f4b9ff8d7dd6cbe39e8b0abaca9d40
Petra <petra.grunheidt@gmail.com>
2022-09-13 12:48:11 -0300

    non functional modifications to controller

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,6 +1,8 @@
 class WarehousesController < ApplicationController
+    before_action :set_warehouse, only: [:show, :edit, :update]
+    
     def show
-        @warehouse = Warehouse.find(params[:id])
+        set_warehouse
     end
 
     def new
@@ -8,8 +10,6 @@ class WarehousesController < ApplicationController
     end
 
     def create
-        warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :area, :address,
-             :cep, :description)
         @warehouse = Warehouse.new(warehouse_params)
         
         if @warehouse.save
@@ -20,18 +20,23 @@ class WarehousesController < ApplicationController
     end
 
     def edit
-        @warehouse = Warehouse.find(params[:id])
     end
 
     def update
-        @warehouse = Warehouse.find(params[:id])
-        warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :area, :address,
-            :cep, :description)
-
         if @warehouse.update(warehouse_params)
             return redirect_to warehouse_path(@warehouse.id), notice: 'Galpão editado com sucesso!'
         end
         render :new
+    end
+    
+    private
 
+    def set_warehouse
+        @warehouse = Warehouse.find(params[:id])
+    end
+
+    def warehouse_params
+        params.require(:warehouse).permit(:name, :code, :city, :area, :address,
+            :cep, :description)
     end
 end
