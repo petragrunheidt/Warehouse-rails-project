677b2dd58e6a6858ed584dd833851152d0e93c2d
Petra <petra.grunheidt@gmail.com>
2022-09-19 13:26:44 -0300

    adds uniqueness validation do CNPJ

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,5 +1,6 @@
 class Supplier < ApplicationRecord
     validates :corporate_name, :brand_name, :registration_number,
      :city, :state, :email, :address, presence: true
+    validates :registration_number, uniqueness: true
     validates :registration_number, length: { is: 13 }
 end
--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -15,5 +15,20 @@ RSpec.describe Supplier, type: :model do
         expect(result).to eq false
       end
     end
+    context 'uniqueness' do
+      it 'false when registering a duplicated registration number' do
+        # Arrange
+        Supplier.create!(corporate_name: 'PETR ltda', brand_name: 'Petra', registration_number: '3910392039401',
+          city: 'Petropolis', state: 'SP', email: 'petramail', address: 'Rua da Petra')
+        sup2 = Supplier.new(corporate_name: 'PMN ltda', brand_name: 'Pumuna', registration_number: '3910392039401',
+          city: 'Pumunopolis', state: 'MG', email: 'pumunamail', address: 'Rua da Pumuna')
+
+        # Act
+        result = sup2.valid?
+
+        # Assert
+        expect(result).to eq false
+      end
+    end
   end
 end
